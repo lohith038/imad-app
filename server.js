0718ef785b004ff23437c20ca99e3c947d7b8cba
lohith038 <30753186+lohith038@users.noreply.github.com>
2017-08-07 07:00:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,16 +90,9 @@ app.get('/articleName', function (req, res) {
   //articleName == article-one
   //articles(articleName) == {} content for article one
   var articleName = res.params.articleName;
-  res.send(createTemplate(articles(articleName)));
+  res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
