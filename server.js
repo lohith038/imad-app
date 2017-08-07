bd95defa47652fa5d88e1b0d923a8a89e8b3aa97
lohith038 <30753186+lohith038@users.noreply.github.com>
2017-08-07 07:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get('/', function (req, res) {
 app.get('/articleName', function (req, res) {
   //articleName == article-one
   //articles(articleName) == {} content for article one
-  var articleName = res.params.articleName;
+  var articleName = res.parans.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
