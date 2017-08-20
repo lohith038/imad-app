0dd997d1ece70bc5aa6118f79ae42e6071792b52
lohith038 <30753186+lohith038@users.noreply.github.com>
2017-08-20 14:21:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,25 @@ var htmlTemplate= `
    </title>
    <metaname="viewreport" content="width-device-width, initial-scale=1"/>
    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+ <body>
+     <div class="container">
+        <div>
+        <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+    </div>    
+ </body>
+ </html>
 
 `;
 
