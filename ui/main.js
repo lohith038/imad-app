8665095c6693bffbc15c893ddbf464dd0925a409
lohith038 <30753186+lohith038@users.noreply.github.com>
2017-08-20 15:21:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function(){
 };
 
 //submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
@@ -52,15 +50,8 @@ submit.onclick=function(){
     };
     
     // make the request
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET','http://lohith038.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
-  
-  //capture a list of names and render it as a list
-  var names=['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list+='<li>'+names[i]+'</li>';
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
 };
