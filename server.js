4c8886631ed188cb5bde33cb93a0ba474b6ac766
lohith038 <30753186+lohith038@users.noreply.github.com>
2017-08-20 13:51:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+var express = require('express');
+var morgan= require('morgan');
+var path = require('path');
+
+
+var app=express();
+app.use(morgan('combined'));
+
 app.get('/',function (req, res){
    res.sendFile(path.join(__dirname,'ui','index.html')); 
 });
@@ -19,7 +27,7 @@ app.get('/ui/madi.png',function (req, res){
    res.sendFile(path.join(__dirname,'ui','madi.png')); 
 });
 
-var port=80;
-app.listen(port, function(){
-    console.log('IMAD course app listening on port  ${port}!');
+var port=8080;
+app.listen(8080, function(){
+    console.log(`IMAD course app listening on port  ${port}!`);
 });
